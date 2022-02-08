03d4b7f188502d78f8acddad85e8cc16e731d16d
Kui Kévin <Kui Kvin@DESKTOP-QKLVE4D>
2022-02-08 12:57:50 +0100

    Ajoutez des fichiers projet.

---
+++ Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Scientifique_Info_Kevin_Brieuc
+{
+    internal class MyImage
+    {
+
+
+
+
+
+
+        public MyImage(string myfile)
+        {
+
+        }
+
+        public void From_Image_To_File(string file)
+        {
+
+        }
+
+        public int Convertir_Endian_To_Int(byte[] tab )
+        {
+            int a = 0;
+            return a;
+        }
+        public byte[] Convertir_Int_To_Endian(int val )
+        {
+            byte[] a = null;
+            return a;
+        }
+
+
+
+    }
+}
---
+++ Projet_Scientifique_Info_Kevin_Brieuc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Scientifique_Info_Kevin_Brieuc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
