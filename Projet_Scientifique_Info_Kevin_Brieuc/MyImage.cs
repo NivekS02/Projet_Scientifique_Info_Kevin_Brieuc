c99f4266646aa108f6b4ee4d47018bb964be42b1
Kévin KUI <kevin.kui@edu.devinci.fr>
2022-02-08 14:12:05 +0100

    modifications 1

--- Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
+++ Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Projet_Scientifique_Info_Kevin_Brieuc
 {
@@ -19,28 +20,27 @@ namespace Projet_Scientifique_Info_Kevin_Brieuc
 
 	public MyImage(string myfile)
     {
-        #region LectureImage
-        byte[] myfile = File.ReadAllBytes(myfile);
+        byte[] file = File.ReadAllBytes(myfile);
         //myfile est un vecteur composé d'octets représentant les métadonnées et les données de l'image
 
 
         //lecture du header et assignement des variables d'instance       
-        if (myfile[0] == 66 && myfile[1] == 77)
+        if (file[0] == 66 && file[1] == 77)
         {
             this.typeImage = "BM";
         }
-        this.tailleFichier = myfile[2] * 1 + myfile[3] * 256 + myfile[4] * 65536 + myfile[5] * 16777216;
-        this.largeur = myfile[18] * 1 + myfile[19] * 256 + myfile[20] * 65536 + myfile[21] * 16777216;
-        this.hauteur = myfile[22] * 1 + myfile[23] * 256 + myfile[24] * 65536 + myfile[25] * 16777216;
-        this.tailleOffset = myfile[14] * 1 + myfile[15] * 256 + myfile[16] * 65536 + myfile[17] * 16777216;
-        this.bitsParCouleur = myfile[28] * 1 + myfile[29] * 256;
+        this.tailleFichier = file[2] * 1 + file[3] * 256 + file[4] * 65536 + file[5] * 16777216;
+        this.largeur = file[18] * 1 + file[19] * 256 + file[20] * 65536 + file[21] * 16777216;
+        this.hauteur = file[22] * 1 + file[23] * 256 + file[24] * 65536 + file[25] * 16777216;
+        this.tailleOffset = file[14] * 1 + file[15] * 256 + file[16] * 65536 + file[17] * 16777216;
+        this.bitsParCouleur = file[28] * 1 + file[29] * 256;
 
         int k = 54;
         for (int i = 0; i < this.hauteur; i++)
         {
             for (int j = 0; j < this.largeur; j++)
             {
-                this.image[i, j] = myfile[k];
+                this.image[i, j] = file[k];
                 k++;
             }
         }
