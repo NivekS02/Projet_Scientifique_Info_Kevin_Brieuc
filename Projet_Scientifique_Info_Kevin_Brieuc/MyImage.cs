fb93b5c1da16ab9e08ee82c3e65ee2cc7e3dc9a5
Brieuc LE GUÉVEL <brieuc.le_guevel@edu.devinci.fr>
2022-02-08 13:20:41 +0100

    Ajout Brieuc

--- Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
+++ Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
@@ -8,27 +8,58 @@ namespace Projet_Scientifique_Info_Kevin_Brieuc
 {
     internal class MyImage
     {
+	private string typeImage;
+	private int tailleFichier;
+	private int tailleOffset
+	private int hauteur;
+	private int largeur;
+	private int bitsParCouleur;
+	private byte[,] image;
 
-        public MyImage(string myfile)
+
+	public MyImage(string myfile)
+    {
+        #region LectureImage
+        byte[] myfile = File.ReadAllBytes(myfile);
+        //myfile est un vecteur composé d'octets représentant les métadonnées et les données de l'image
+
+
+        //lecture du header et assignement des variables d'instance       
+        if (myfile[0] == 66 && myfile[1] == 77)
         {
-
+            this.typeImage = "BM";
         }
+        this.tailleFichier = myfile[2] * 1 + myfile[3] * 256 + myfile[4] * 65536 + myfile[5] * 16777216;
+        this.largeur = myfile[17] * 1 + myfile[18] * 256 + myfile[19] * 65536 + myfile[20] * 16777216
+        this.hauteur = myfile[21] * 1 + myfile[22] * 256 + myfile[23] * 65536 + myfile[24] * 16777216;
+        this.tailleOffset = myfile[13] * 1 + myfile[14] * 256 + myfile[15] * 65536 + myfile[16] * 16777216;
+        this.bitsParCouleur = myfile[27] * 1 + myfile[28] * 256;
 
-        public void From_Image_To_File(string file)
+        int k = 54;
+        for (int i = 0; i < this.hauteur; i++)
         {
+            for (int j = 0; j < this.largeur; j++)
+            {
+                this.image[i, j] = myfile[k];
+                k++;
+            }
+        }
+    }
 
-        }
+    
+    public void From_Image_To_File(string file)
+    {
 
-        public int Convertir_Endian_To_Int(byte[] tab )
-        {
-            int a = 0;
-            return a;
-        }
-        public byte[] Convertir_Int_To_Endian(int val )
-        {
-            byte[] a = null;
-            return a;
-        }
+    }
+	public int Convertir_Endian_To_Int(byte[] tab)
+    {
+
+    }
+	public byte[] Convert_Int_To_Endian(int val)
+    {
+		
+    }
+    
 
 
 
