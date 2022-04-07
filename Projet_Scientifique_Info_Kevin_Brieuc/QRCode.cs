89ee170e850fbc7587247dbea9ad91cd7774d49a
Brieuc LE GUÉVEL <brieuc.le_guevel@edu.devinci.fr>
2022-04-07 17:44:59 +0200

    maj qrcode

---
+++ Projet_Scientifique_Info_Kevin_Brieuc/QRCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Projet_Scientifique_Info_Kevin_Brieuc
+{
+    internal class QRCode
+    {
+
+    }
+}
