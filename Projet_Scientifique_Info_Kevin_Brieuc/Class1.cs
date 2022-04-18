470e10bf4458cfb211f6f4d48201f1fa8cfe0b50
Kévin KUI <kevin.kui@edu.devinci.fr>
2022-04-18 12:02:31 +0200

    yo

---
+++ Projet_Scientifique_Info_Kevin_Brieuc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Scientifique_Info_Kevin_Brieuc
+{
+    internal class Class1
+    {
+    }
+}
