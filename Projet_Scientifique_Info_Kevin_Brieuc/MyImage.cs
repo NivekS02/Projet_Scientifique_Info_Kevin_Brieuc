ae902004e7e859498795571fcc6921da7c363f79
Brieuc LE GUÉVEL <brieuc.le_guevel@edu.devinci.fr>
2022-04-25 12:44:51 +0200

    maj

--- Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
+++ Projet_Scientifique_Info_Kevin_Brieuc/MyImage.cs
@@ -928,7 +928,7 @@ namespace Projet_Scientifique_Info_Kevin_Brieuc
         {
             for (int i = 0; i < image.GetLength(0); i++)
             {
-                for (int j = 0; i < image.GetLength(1); i++)
+                for (int j = 0; j < image.GetLength(1); j++)
                 {
                     image[i, j].B = BinaireToByte(InversionTableauBinaire(ByteToBinaire(image[i, j].B)));
                     image[i, j].R = BinaireToByte(InversionTableauBinaire(ByteToBinaire(image[i, j].R)));
